c75e1849ba0a68885968f3e56ff5e63f4db2331b
GeunTae KIM <k031900@gmail.com>
2021-11-28 10:06:13 +0900

    2

--- Assets/Script/AdmobBannerScript.cs
+++ Assets/Script/AdmobBannerScript.cs
@@ -16,7 +16,8 @@ public class AdmobBannerScript : MonoBehaviour
     {
         //Test Banner ID : ca-app-pub-3940256099942544/6300978111
         //여러분들의 광고 ID가 들어갈 곳입니다.
-        string BannerID = "ca-app-pub-3940256099942544/6300978111";
+        string BannerID = "ca-app-pub-9811785555570025/2291827743";
+
         BannerView bannerview = new BannerView(BannerID, AdSize.Banner, AdPosition.Bottom);
 
         AdRequest request = new AdRequest.Builder().Build();
